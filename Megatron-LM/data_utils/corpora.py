f2f15a5f2061b371e2e86bb9bfe6103cd42f106f
Chameleon Cloud User <cc@v100-3.novalocal>
2021-01-10 18:31:48 +0000

    update corpora.py

--- Megatron-LM/data_utils/corpora.py
+++ Megatron-LM/data_utils/corpora.py
@@ -22,7 +22,8 @@ class wikipedia(json_dataset):
 
     command line usage: `--train-data wikipedia`
     """
-    PATH = '/home/cc/DeepSpeed/DeepSpeedExamples/Megatron-LM/train_data.json'
+    #PATH = '/home/cc/DeepSpeed/DeepSpeedExamples/Megatron-LM/train_data.json'
+    PATH = 'data/wikipedia/wikidump_lines.json'
     assert_str = "make sure to set PATH for wikipedia data_utils/corpora.py"
     def __init__(self, **kwargs):
         assert os.path.exists(wikipedia.PATH), \
@@ -40,7 +41,7 @@ class webtext(json_dataset):
 
     command line usage: `--train-data webtext`
     """
-    PATH = '/home/cc/DeepSpeed/DeepSpeedExamples/Megatron-LM/train_data.json'
+    PATH = 'data/webtext/data.json'
     assert_str = "make sure to set PATH for webtext data_utils/corpora.py"
     def __init__(self, **kwargs):
         assert os.path.exists(webtext.PATH), \
